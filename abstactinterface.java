6c46a729cfba69461500d3004f385405cbf66dc3
manojmahi90526 <23hr1a3012@mtieat.org>
2024-12-12 13:41:19 +0530

    Add files via upload

---
+++ abstactinterface.java
@@ -0,0 +1,18 @@
+import java.util.*;
+abstract interface math{
+    void print();
+
+} 
+class d implements math{
+    public void print(){
+        System.out.println("hello");
+    
+    }
+}
+class main{
+    public static void main(String args[]){
+        d f=new d();
+        f.print();
+
+    }
+}
